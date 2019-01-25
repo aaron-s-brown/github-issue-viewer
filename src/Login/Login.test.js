5e81d2d4047f81850ee8bbf403a4f99e7912e568
Aaron Brown <aaronstewartbrown@gmail.com>
2019-01-24 17:41:08 -0800

    Add Login snapshot test

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import {ShallowLogin} from './Login';
+import renderer from 'react-test-renderer';
+
 
 describe('<Login>', () => {
+
+  it('renders correctly', () => {
+    const submitMock = jest.fn();
+    const tree = renderer
+      .create(<ShallowLogin setToken={submitMock}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   it('should call setToken prop on form submit', () => {
     const submitMock = jest.fn();
     const login = shallow(<ShallowLogin setToken={submitMock}/>);
